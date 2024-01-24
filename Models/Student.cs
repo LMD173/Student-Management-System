d8d3e29ccd2d4e7426cb9da238262735ecc22a04
Leo <155098875+LMD173@users.noreply.github.com>
2024-01-24 12:08:15 +0000

    Update Student.cs

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,6 +1,18 @@
 namespace StudentManagementSystem.Models;
 
+/// <summary>
+/// Represents a student.
+/// </summary>
 public class Student
 {
+    public int Id { get; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public float Height { get; set; }
 
+    public Student(int id)
+    {
+        Id = id;
+    }
 }
