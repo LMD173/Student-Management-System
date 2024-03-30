86f26a952cb15a4dba549e453db7bd170858d2d4
Leo <155098875+LMD173@users.noreply.github.com>
2024-03-30 20:39:25 +0000

    Add input method

--- Utilities/Logger.cs
+++ Utilities/Logger.cs
@@ -4,47 +4,53 @@ public class Logger
 {
 
     /// <summary>
+    /// Logs an input message.
+    /// </summary>
+    /// <param name="message">the input message to output.</param>
+    /// <param name="newLine">whether to add a new line after the message.</param>
+    public static void Input(string message, bool newLine = false)
+    {
+        Log($"{message} >>> ", ConsoleColor.Cyan, newLine);
+    }
+
+    /// <summary>
     /// Logs an informational message.
     /// </summary>
     /// <param name="message">the message to output.</param>
-    public static void Info(string message)
+    /// <param name="newLine">whether to add a new line after the message.</param>
+    public static void Info(string message, bool newLine = true)
     {
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine($"[INFO] {message}");
-        Console.ResetColor();
+        Log($"[INFO] {message}", ConsoleColor.White, newLine);
     }
 
     /// <summary>
     /// Logs an error message.
     /// </summary>
     /// <param name="message">the error message to output.</param>
-    public static void Error(string message)
+    /// <param name="newLine">whether to add a new line after the message.</param>
+    public static void Error(string message, bool newLine = true)
     {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"[ERROR] {message}");
-        Console.ResetColor();
+        Log($"[ERROR] {message}", ConsoleColor.Red, newLine);
     }
 
     /// <summary>
     /// Logs a success message.
     /// </summary>
     /// <param name="message">the success message to output.</param>
-    public static void Success(string message)
+    /// <param name="newLine">whether to add a new line after the message.</param>
+    public static void Success(string message, bool newLine = true)
     {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"[SUCCESS] {message}");
-        Console.ResetColor();
+        Log($"[SUCCESS] {message}", ConsoleColor.Green, newLine);
     }
 
     /// <summary>
     /// Logs a warning message.
     /// </summary>
     /// <param name="message">the warning message to output.</param>
-    public static void Warning(string message)
+    /// <param name="newLine">whether to add a new line after the message.</param>
+    public static void Warning(string message, bool newLine = true)
     {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine($"[WARNING] {message}");
-        Console.ResetColor();
+        Log($"[WARNING] {message}", ConsoleColor.Yellow, newLine);
     }
 
     /// <summary>
@@ -54,9 +60,27 @@ public class Logger
     /// <param name="code">an optional code to exit the environment with.</param>
     public static void Fatal(string message, int code = 1)
     {
-        Console.ForegroundColor = ConsoleColor.DarkRed;
-        Console.WriteLine($"[FATAL] {message}");
-        Console.ResetColor();
+        Log($"[FATAL] {message}", ConsoleColor.Red);
         Environment.Exit(code);
+    }
+
+    /// <summary>
+    /// Logs a message to the console.
+    /// </summary>
+    /// <param name="message">the message to log to the console.</param>
+    /// <param name="color">the console colour to use.</param>
+    /// <param name="newLine">whether to add a new line after the message.</param>
+    public static void Log(string message, ConsoleColor color = ConsoleColor.White, bool newLine = true)
+    {
+        Console.ForegroundColor = color;
+        if (newLine)
+        {
+            Console.WriteLine(message);
+        }
+        else
+        {
+            Console.Write(message);
+        }
+        Console.ResetColor();
     }
 }
